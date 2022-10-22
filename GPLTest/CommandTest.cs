a853a1e4e36a5502bc33d3e91ebc2c8cc4950f99
HuzaifaHassan <huzaifahassan97@outlook.com>
2022-10-22 18:02:18 +0100

    UnitTest for Command

---
+++ GPLTest/CommandTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GPLTest
+{
+    [TestClass]
+    public class CommandTest
+    {
+        [TestMethod]
+        public void testIfStatments()
+        {
+            try
+            {
+                int a = 10;
+                int b = 20;
+                string symbol = "=";
+                if (symbol == "=")
+                { 
+                  Assert.AreEqual(a, b,00.1,"Number match successful");
+                
+                }
+                else
+                {
+                    Assert.AreNotEqual(a, b, 00.1, "Number match unsuccessfull");
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Symbol Error");
+            }
+        }
+    }
+}
