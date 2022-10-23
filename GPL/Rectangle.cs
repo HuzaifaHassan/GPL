98576e27f1568fa539a8c023c32c660c3481fef4
HuzaifaHassan <huzaifahassan97@outlook.com>
2022-10-23 16:35:47 +0100

    Rectangle Class..

--- GPL/Rectangle.cs
+++ GPL/Rectangle.cs
@@ -9,6 +9,23 @@ namespace GPL
 {
     public class Rectangle : Shape
     {
+        int _width, _height;
+        public Rectangle() : base()
+        { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_color"></param>
+        /// <param name="_x"></param>
+        /// <param name="_y"></param>
+        /// <param name="width_"></param>
+        /// <param name="height_"></param>
+        public Rectangle(Color _color,int _x,int _y,int width_,int height_) : base (_color,_x,_y)
+        { 
+            this._width = width_;
+            this._height = height_;     
+        }
         public override void draw(Graphics g)
         {
             throw new NotImplementedException();
